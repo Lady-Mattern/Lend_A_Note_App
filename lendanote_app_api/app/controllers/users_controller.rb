13e37d34f4dd62322e1f20c36079a197d3ae7bca
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-07-27 19:41:41 -0400

    fixed another mistake or two in the users controller file

--- lendanote_app_api/app/controllers/users_controller.rb
+++ lendanote_app_api/app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
     def create 
         user = User.create(
-            name: params[:name]
-            school: params[:school]
-            region: params[:region]
-            email: params[:email]
-            phone: params[:phone]
-            password: params[:password]
+            name: params[:name],
+            school: params[:school],
+            region: params[:region],
+            email: params[:email],
+            phone: params[:phone],
+            password: params[:password],
         )
-    end
         render json: user
+    end
 end
