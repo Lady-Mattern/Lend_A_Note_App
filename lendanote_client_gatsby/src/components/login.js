b82d52a7d7c0fad5dcd7ca3b02233528ac26e004
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-08-03 23:21:44 -0400

    Added user authorization and authentication to services/auth.js

--- lendanote_client_gatsby/src/components/login.js
+++ lendanote_client_gatsby/src/components/login.js
@@ -5,13 +5,13 @@ import { render } from 'react-dom';
 
 const Login = () => (
     state = {
-        email: '',
-        password: ''
+        email: ``,   
+        password: ``, 
     }
 
     handleUpdate = event => {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
         })
     }
 
@@ -24,26 +24,73 @@ const Login = () => (
         if (isLoggedIn()) {
             navigate(`/app/profile`)
         }
-    }
 
-    return (
-        <>
-            <form 
-                method="POST"
-                onSubmit={event => {
-                    this.handleSubmit(event)
-                    navigate(`/app/profile`)
-                }}    
-            >
-                <label htmlFor="email">Email: </label>
-                    <input type="text" id="email" name="email" onChange={this.handleUpdate} />
-                <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" name="password" onChange={this.handleUpdate} />
-                <input type="submit" value="Log In" />
-            </form>        
-        </>
-    )
-
+        return (
+            <div>
+                <form 
+                    method="POST"
+                    onSubmit={event => {
+                        this.handleSubmit(event)
+                        navigate(`/app/profile`)
+                    }}    
+                >
+                    <label htmlFor="email">Email: </label>
+                        <input type="text" id="email" name="email" onChange={this.handleUpdate} />
+                    <label htmlFor="password">Password: </label>
+                        <input type="password" id="password" name="password" onChange={this.handleUpdate} />
+                    <input type="submit" value="Log In" />
+                </form>        
+            </div>
+        )
+    }              
 )
+
+
+// class Login extends React.Component {
+//     state = {
+//       email: ``,
+//       password: ``,
+//     }
+//     handleUpdate = event => {
+//       this.setState({
+//         [event.target.name]: event.target.value,
+//       })
+//     }
+//     handleSubmit = event => {
+//       event.preventDefault()
+//       handleLogin(this.state)
+//     }
+//     render() {
+//       if (isLoggedIn()) {
+//         navigate(`/app/profile`)
+//       }
+//       return (
+//         <>
+//           <h1>Log in</h1>
+//           <form
+//             method="post"
+//             onSubmit={event => {
+//               this.handleSubmit(event)
+//               navigate(`/app/profile`)
+//             }}
+//           >
+//             <label>
+//               Email
+//               <input type="text" name="email" onChange={this.handleUpdate} />
+//             </label>
+//             <label>
+//               Password
+//               <input
+//                 type="password"
+//                 name="password"
+//                 onChange={this.handleUpdate}
+//               />
+//             </label>
+//             <input type="submit" value="Log In" />
+//           </form>
+//         </>
+//       )
+//     }
+//   }
 
 export default Login;
--- lendanote_client_gatsby/src/components/nav.js
+++ lendanote_client_gatsby/src/components/nav.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Nav = () => (
+    <div>
+        <span>You are not logged in</span>
         <nav>
-            <Link to="/">Home</Link>
             <Link to="/aboutPage">About</Link>
             <Link to="/signupPage">Sign Up</Link>
             <Link to="/loginPage">Log In</Link>
             <Link to="/userPage">My Page</Link>
         </nav>
+    </div>
 )
 
 export default Nav;
