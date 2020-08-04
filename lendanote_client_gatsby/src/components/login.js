5838e90e2dea000f938ca8da6d95c72ebc8bff91
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-08-03 22:01:10 -0400

    Created login form with state. The submit button is not showing up

--- lendanote_client_gatsby/src/components/login.js
+++ lendanote_client_gatsby/src/components/login.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import LoginPage from '../pages/loginPage'
+import { navigate } from 'gatsby';
+import { handleLogin, isLoggedIn } from '../services/auth';
+import { render } from 'react-dom';
 
 const Login = () => (
-    <form>
-        <label htmlFor="email">Email: </label>
-            <input type="text" id="email" />
-        <label htmlFor="password">Password: </label>
-            <input type="text" id="password" />
-    </form>
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleUpdate = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        handleLogin(this.state)
+    }
+
+    render() {
+        if (isLoggedIn()) {
+            navigate(`/app/profile`)
+        }
+    }
+
+    return (
+        <>
+            <form 
+                method="POST"
+                onSubmit={event => {
+                    this.handleSubmit(event)
+                    navigate(`/app/profile`)
+                }}    
+            >
+                <label htmlFor="email">Email: </label>
+                    <input type="text" id="email" name="email" onChange={this.handleUpdate} />
+                <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password" onChange={this.handleUpdate} />
+                <input type="submit" value="Log In" />
+            </form>        
+        </>
+    )
+
 )
 
 export default Login;
