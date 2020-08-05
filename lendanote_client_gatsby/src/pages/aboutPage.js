785a3b891f04b24ea5ffae960b3e90f573d29ce2
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-08-05 19:26:47 -0400

    Added content to the About page.

--- lendanote_client_gatsby/src/pages/aboutPage.js
+++ lendanote_client_gatsby/src/pages/aboutPage.js
@@ -4,6 +4,55 @@ import Layout from '../components/layout';
 const AboutPage = () => (
         <Layout>
             <h1>About Lend A Note</h1>
+            <p>
+                Lend A Note is an application born from my own experiences as a music educator working in small school districts. 
+            </p>
+            <p>
+                Literature selection is the process by which ensemble directors select music to perform with their ensembles. The music that students are exposed to serves the same purpose as textbooks and worksheets for other academic classes. School ensemble directors use this music to teach musical concepts and performing techniques to their students. In addition, by exposing students to challenging literature from various cultures and time periods, directors allow them to grow as musicians and experience the world around them. With a task as important as selecting what concepts to teach their students, directors may find that literature selection is one of the most challenging and rewarding tasks. 
+            </p>
+            <p>
+                According to H. Robert Reynolds, there are some questions that directors must ask themselves when selecting literature that will challenge and inspire their students.
+            </p>    
+                <ol>
+                    <li>
+                        Will the students enjoy the music?
+                    </li>
+                    <li>
+                        What would the audience want to hear?
+                    </li>
+                    <li>
+                        Is the music too difficult for the ensemble to learn in a reasonable period of time?
+                    </li>
+                    <li>
+                        Will the ensemble have all of the instrumentation that the composition requires?
+                    </li>
+                    <li>
+                        Will there be enough time to learn all of the music?
+                    </li>
+                    <li>
+                        Will the repertoire selected represent enough of a balance between styles, aesthetics, depth, and other factors?
+                    </li>
+                    <li>
+                        Will the students grow as a result of learning the literature selected?
+                    </li>
+                    <li>
+                        Will the director grow for the same reasons?
+                    </li>
+                </ol>
+                <p>
+                    With all of these factors to consider every time they select music for their ensembles, directors may find literature selection to be one of the most daunting tasks they perform. It may come as no surprise that over the years many sources may have been developed to assist directors in locating suitable literature.
+                </p>
+                <p>
+                    Often times, small school districts have small music programs. Bands, choirs, and orchestras, have twelve students or less, but the music in the school's library might be written for ensembles of as many as sixty musicians and for a wide instrumentation. Trying to choose music to help an ensemble grow and learn and match the criteria on H. Robert Reynolds' list may not be an option. Especially if the music program does not have the budget to purchase new music for every ensemble that is suitable.
+                </p>
+                <p>
+                    This is where the Lend A Note application comes in. Teachers may create accounts, and add music from their libraries that they would be willing to share. Teachers from all over the country may then choose from a list of music to borrow for a reasonable period of time without having to spend money that they do not have. They may search by title, composer/arranger, and instrumentation or ensemble type. This should help them locate pieces that best suite their needs.
+                </p>
+
+                <h2>The Future of Lend A Note</h2>
+                <p>
+                    Eventually, it would be wonderful to include a comment section with each pice so teachers can list missing items, their students' reactions to the piece they tried, or if there were any issues with the lending and borrowing process. It would also be interesting to give the lenders and borrowers a rating system, much like you would rate Amazon affiliates, or your Uber driver.
+                </p>
         </Layout>
 )
 
